407cd5f306e70150a74e4e41721fda16875d766e
Barbara Dola <barbara.stepuch@wp.pl>
2022-01-23 14:06:16 +0100

    ManyToManyField

--- Student_lecture_bsd/student_lecture/student_lecture_app/models.py
+++ Student_lecture_bsd/student_lecture/student_lecture_app/models.py
@@ -2,14 +2,15 @@ from django.db import models
 
 # Create your models here.
 class Student(models.Model):
-    name =  models.CharField(max_length=64, unique=True)#string, max. 64 znaki, imię powinno być unikalne
-    year_at_university =  models.IntegerField()#integer,
-    is_active = models.BooleanField(default=True)# boolean, standardowa wartość True,
+    name =  models.CharField(max_length=64, unique=True)
+    year_at_university =  models.IntegerField()
+    is_active = models.BooleanField(default=True)
 
 
 
 class Lecture(models.Model):
     name = models.CharField(max_length=64, unique=True)
     lecturer = models.CharField(max_length=64)
+    students = models.ManyToManyField(Student)
 
 
