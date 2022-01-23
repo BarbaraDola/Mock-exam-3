b149cd29aaae80b13db377e4df8de22c74ad4257
Barbara Dola <barbara.stepuch@wp.pl>
2022-01-23 13:56:55 +0100

    creating models

--- Student_lecture_bsd/student_lecture/student_lecture_app/models.py
+++ Student_lecture_bsd/student_lecture/student_lecture_app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+    name =  models.CharField(max_length=64, unique=True)#string, max. 64 znaki, imię powinno być unikalne
+    year_at_university =  models.IntegerField()#integer,
+    is_active = models.BooleanField(default=True)# boolean, standardowa wartość True,
+
+
+
+class Lecture(models.Model):
+    name = models.CharField(max_length=64, unique=True)
+    lecturer = models.CharField(max_length=64)
+
+
